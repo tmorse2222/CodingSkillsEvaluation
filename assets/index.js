33225168202b8a47e5f8d495c5ff1f6694b88720
Tyler M <tmorse2222@gmail.com>
2022-07-08 11:03:00 -0700

    Created template for question 2; Created JS var for correct and incorrect answers; set up function allowing for replacement of text

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,21 @@
 // Global varriables
+
 var startButton = document.getElementById(`startButton`);
+var correct = document.getElementById(`correct`);
+var incorrect = document.getElementById(`incorrect`);
+
 // Created Functions
+
 function start() {
-    document.getElementById(`startContain`).style.display = "none";
-    document.getElementById(`q1`).style.display = "flex";
+    document.getElementById(`startContain`).style.display = `none`;
+    document.getElementById(`q1`).style.display = `flex`;
+};
+function q2() {
+    document.getElementById(`q1`).style.display = `none`;
+    document.getElementById(`q2`).style.display = `flex`;
 };
 
+// Declared Functions
+
 startButton.onclick = start;
+correct.onclick = q2;
