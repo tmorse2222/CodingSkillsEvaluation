8d3991e88c31f6441b163dfb66a2d013e72f47cb
Tyler M <tmorse2222@gmail.com>
2022-07-08 12:57:22 -0700

    Bug fixes for JS, allwing clicks for more than first child; some work on q3

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,8 @@
 // Global varriables
 
 var startButton = document.getElementById(`startButton`);
-var correct = [document.getElementById(`correct`)];
-var incorrect = [document.getElementById(`incorrect`)];
+var correct2 = document.getElementById(`correct2`);
+var incorrect2 = document.getElementById(`incorrect2`);
 
 // Created Functions
 
@@ -14,9 +14,19 @@ function q2() {
     document.getElementById(`q1`).style.display = `none`;
     document.getElementById(`q2`).style.display = `flex`;
 };
+function q3() {
+    document.getElementById(`q2`).style.display = `none`;
+    document.getElementById(`q3`).style.display = `flex`;
+};
 
 // Declared Functions
 
 startButton.onclick = start;
-correct[0].onclick = q2;
-incorrect[0].onclick = q2;
+
+document.getElementById(`c1`).onclick = q2;
+document.getElementById(`i11`).onclick = q2;
+document.getElementById(`i12`).onclick = q2;
+document.getElementById(`i13`).onclick = q2;
+
+correct2.onclick = q3;
+incorrect2.onclick = q3;
