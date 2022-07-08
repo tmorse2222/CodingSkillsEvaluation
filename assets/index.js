eb1b612b605c9aaffbe4c2423ecad39d2474b054
Tyler M <tmorse2222@gmail.com>
2022-07-07 20:24:21 -0700

    Centered first question on apperance

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@ var startButton = document.getElementById(`startButton`);
 // Created Functions
 function start() {
     document.getElementById(`startContain`).style.display = "none";
-    document.getElementById(`q1`).style.display = "unset";
+    document.getElementById(`q1`).style.display = "flex";
 };
 
 startButton.onclick = start;
