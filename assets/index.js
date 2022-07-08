ed70ca8d3ee20c63e8ffbdd57c2a153a32b96016
Tyler M <tmorse2222@gmail.com>
2022-07-08 13:00:57 -0700

    Increased functionality to q2

--- assets/index.js
+++ assets/index.js
@@ -28,5 +28,7 @@ document.getElementById(`i11`).onclick = q2;
 document.getElementById(`i12`).onclick = q2;
 document.getElementById(`i13`).onclick = q2;
 
-correct2.onclick = q3;
-incorrect2.onclick = q3;
+document.getElementById(`c2`).onclick = q3;
+document.getElementById(`i21`).onclick = q3;
+document.getElementById(`i22`).onclick = q3;
+document.getElementById(`i23`).onclick = q3;
